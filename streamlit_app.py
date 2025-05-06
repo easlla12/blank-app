1ff1e125a2b21fee05d220a28c55487edc45da83
easlla12 <asllanajesmeralda@gmail.com>
2025-05-06 05:59:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 import numpy as np
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+
 import joblib  # For loading preprocessor if saved separately
 
 
