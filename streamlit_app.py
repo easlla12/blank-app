dceeb49a0942a1bfad9bbdb9f7716d5598fdb284
easlla12 <asllanajesmeralda@gmail.com>
2025-05-06 05:30:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ st.sidebar.write('''2. Press the **'Predict'** button and wait for the result.''
 #Main page
 col1, col2 = st.columns(2)
 with col1:
-   st.image("logo.png", width=300)
+  
 with col2:
    st.title("Loan Approval Prediction")
    
